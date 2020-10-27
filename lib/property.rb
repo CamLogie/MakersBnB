f8b221394c8e19513720a2732b60bcc17d1a2795
Maggie Hibberd <maggiehibberd@gmail.com>
2020-10-27 14:44:36 +0000

    datebase connected and tested

--- lib/property.rb
+++ lib/property.rb
@@ -1,9 +1,12 @@
+require 'pg'
+
 class Property
 
   def self.all
+    connection = PG.connect(dbname:'makersbnb')
+    result = connection.exec("SELECT * FROM properties;")
+    result.map {|property| property['listing_title']}
 
-    ['Test Property', 'Example Property', 'Fake Property']
-  
   end
 
 end
