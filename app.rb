11bc5644f12970de1191eb7e6d9773a4f800c51a
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-27 10:41:32 +0000

    Added view file for renting_or_hosting and updated controller

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/sign_up' do
+    session[:name] = params[:name]
+    session[:user_name] = params[:user_name]
+    redirect '/renting_or_hosting'
+  end
+
+  get '/renting_or_hosting' do
+    erb :renting_or_hosting
   end
 
 
--- spec/features/users_signup_spec.rb
+++ spec/features/users_signup_spec.rb
@@ -5,5 +5,13 @@ feature 'Homepage' do
     expect(page).to have_content 'Welcome to MakersBnB'
   end
 
+  scenario 'enables the user to sign up' do
+    visit '/'
+    fill_in(:name, with: 'Bob')
+    fill_in(:user_name, with: 'Bob_007')
+    click_button('Submit')
+    expect(page).to have_content 'Renting or Hosting?'
+  end
+
 
 end
