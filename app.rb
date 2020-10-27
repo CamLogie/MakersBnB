bf265a960155239d2d5787346d76f7dd0cacb08e
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-27 10:43:34 +0000

    Added comments into app file for reference

--- app.rb
+++ app.rb
@@ -4,16 +4,19 @@ class MakersBnB < Sinatra::Base
 
   enable :sessions
 
+  # Sign up form/function
   get '/' do
     erb :index
   end
 
+  # This will collect the user info.
   post '/sign_up' do
     session[:name] = params[:name]
     session[:user_name] = params[:user_name]
     redirect '/renting_or_hosting'
   end
 
+  # This is the landing page for renting or hosting
   get '/renting_or_hosting' do
     erb :renting_or_hosting
   end
