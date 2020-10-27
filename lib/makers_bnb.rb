5905d0f12fb2d8e4908cf3d3ef2b988c81a28faa
KC Thomas <thisiskcthomas@gmail.com>
2020-10-27 11:17:17 +0000

    Added signup method to MakersBnB class and passing unit test.

--- lib/makers_bnb.rb
+++ lib/makers_bnb.rb
@@ -1,3 +1,6 @@
 class MakersBnB
 
+  def self.sign_up(name:, user_name:) 
+    { name: name, user_name: user_name }
+  end
 end
--- spec/unit_tests/makers_bnb_spec.rb
+++ spec/unit_tests/makers_bnb_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../../lib/makers_bnb.rb'
 
 describe MakersBnB do 
+  
+  describe '.sign_up' do
+    it "returns the user's name and user_name" do
+      details = MakersBnB.sign_up(name: 'Bob', user_name: 'bob_007')
 
+      expect(details).to be_a Hash
+      expect(details[:name]).to eq "Bob"
+      expect(details[:user_name]).to eq "bob_007"
+    end
+  end
 end
