cd26e14b57167d32fbc8b7f5825838ea1c55e4bc
Cameron Logie <cameronlogie24@gmail.com>
2020-10-28 09:45:44 +0000

    fixed web_helper/helpers

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,15 @@
 def test_properties
-  connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Fake Property', 'This property is a lovely fake property brought to you by fake property ltd situated in fake, fakeland')")
-  connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Example Property', 'This property is a lovely example property brought to you by example property enterprises situated in example, exampleland')")
-  connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Test Property', 'This property is a lovely test property brought to you by test property co situated in test, testland')")
+  test_connection
+  @connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Fake Property', 'This property is a lovely fake property brought to you by fake property ltd situated in fake, fakeland')")
+  @connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Example Property', 'This property is a lovely example property brought to you by example property enterprises situated in example, exampleland')")
+  @connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('Test Property', 'This property is a lovely test property brought to you by test property co situated in test, testland')")
+end
+
+def test_connection
+  @connection = PG.connect :dbname => 'makers_bnb_manager_test'
+end
+
+def test_add_user
+  test_connection
+  @connection.exec("INSERT INTO users (name, user_name) VALUES ('Bob', 'bob_009');")
 end
