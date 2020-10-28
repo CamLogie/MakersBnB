8cc38bc02c36d082e35b7387cd1a543e1fe86002
KC Thomas <thisiskcthomas@gmail.com>
2020-10-28 11:01:34 +0000

    Added functionality to Avail. class to show current month

--- lib/availability.rb
+++ lib/availability.rb
@@ -1,3 +1,11 @@
+# ------
+
+# Date class that saves/formats and wraps date availability from Property Database
+
 class Availability
+
+  def self.show_month
+    Date.today.month
+  end
 
 end
--- spec/unit/availability_spec.rb
+++ spec/unit/availability_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../../lib/availability'
 
 describe Availability do
+  let(:date) { Date.today }
+  let(:month) { date.month }
+  let(:year) { date.year }
+
+  it 'shows the current month' do 
+    expect(Availability.show_month).to eq month
+  end
 
 end
