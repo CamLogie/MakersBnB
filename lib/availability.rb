1e0ac3bc255d93057888fbe300495316ac546a2f
KC Thomas <thisiskcthomas@gmail.com>
2020-10-28 12:11:33 +0000

    Added functionality to Availability class to create an availability array

--- lib/availability.rb
+++ lib/availability.rb
@@ -3,9 +3,32 @@
 # Date class that saves/formats and wraps date availability from Property Database
 
 class Availability
+  THIS_MONTH = Date.today.month
+  THIS_YEAR = Date.today.year
 
-  def self.show_month
-    Date.today.month
+  attr_reader :availability
+
+  def initialize(availability)
+    @availability = availability
+  end
+
+  def self.available_dates(date)
+    @date = Date.parse(date)
+    @first_day = Date.new(THIS_YEAR, THIS_MONTH, 1)
+    @last_day = Date.new(THIS_YEAR, THIS_MONTH, -1)
+    availability = []
+
+    if date_included?
+      (@first_day..@last_day).each do |day| 
+        availability << day unless day == @date
+      end
+    end
+
+    Availability.new(availability)
+  end
+
+  def self.date_included?
+    (@first_day..@last_day).cover?(@date)
   end
 
 end
--- spec/unit/availability_spec.rb
+++ spec/unit/availability_spec.rb
@@ -1,12 +1,16 @@
 require_relative '../../lib/availability'
 
 describe Availability do
-  let(:date) { Date.today }
-  let(:month) { date.month }
-  let(:year) { date.year }
+  let(:today) { Date.today }
+  let(:month) { today.month }
+  let(:year) { today.year }
 
-  it 'shows the current month' do 
-    expect(Availability.show_month).to eq month
+  it 'takes in one unavailable and creates an available range' do
+    today_formatted = today.strftime('%Y-%m-%d')
+    new_range = Availability.available_dates(today_formatted)
+    
+    expect(new_range.availability).to be_a Array 
+    expect(new_range.availability).not_to include(today)
   end
 
 end
