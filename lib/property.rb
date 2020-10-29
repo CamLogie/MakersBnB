a5aa255e3e3b21eec9fcb04228c77254dd67ba7a
Cameron Logie <cameronlogie24@gmail.com>
2020-10-29 11:12:10 +0000

    listing title and description can include apostrophes

--- lib/property.rb
+++ lib/property.rb
@@ -6,36 +6,47 @@ class Property
 
   def self.all
 
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname:'makers_bnb_manager_test')
-    else
-      connection = PG.connect(dbname:'makers_bnb_manager')
-    end
+    environment_var
 
-    result = connection.exec("SELECT * FROM properties;")
+    result = @@connection.exec("SELECT * FROM properties;")
     result.map {|property| Property.new(id: property['id'], title: property['listing_title'], description: property['listing_description']) }
 
   end
 
   def self.add(listing_title, listing_description)
 
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname:'makers_bnb_manager_test')
-    else
-      connection = PG.connect(dbname:'makers_bnb_manager')
-    end
+    environment_var
 
-    result = connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('#{listing_title}', '#{listing_description}') 
+    listing_title = format_apostrophes(listing_title)
+    listing_description = format_apostrophes(listing_description)
+
+    result = @@connection.exec("INSERT INTO properties (listing_title, listing_description) VALUES('#{listing_title}', '#{listing_description}') 
     RETURNING id, listing_title, listing_description")
-
     Property.new(id: result[0]['id'], title: result[0]['listing_title'], description: result[0]['listing_description'])
 
   end
+
+  def self.environment_var
+    if ENV['RACK_ENV'] == 'test'
+      @@connection = PG.connect(dbname:'makers_bnb_manager_test')
+    else
+      @@connection = PG.connect(dbname:'makers_bnb_manager')
+    end
+  end
+
+  def self.format_apostrophes(string)
+    string.split("'").join("''")
+  end
+    
+  
+
+
 
   def initialize(id:, title:, description:)
     @id = id
     @title = title
     @description = description
   end
+
 
 end
--- spec/features/add_properties_spec.rb
+++ spec/features/add_properties_spec.rb
@@ -8,4 +8,14 @@ feature 'Add new property' do
     expect(page).to have_content 'Test Property'
     expect(page).to have_content 'This property is a lovely test property brought to you by test property co situated in test, testland'
   end
+
+  scenario 'A user can add a new property title and description with as many apostrophes as they\'d like' do 
+    test_connection
+    visit('properties/add_new')
+    fill_in(:listing_title, with: "Test's Property")
+    fill_in(:listing_description, with: "This property is a lovely test property brought to you by test property co situated in tes't, testland")
+    click_button('Submit')
+    expect(page).to have_content "Test's Property"
+    expect(page).to have_content "This property is a lovely test property brought to you by test property co situated in tes't, testland"
+  end
 end
