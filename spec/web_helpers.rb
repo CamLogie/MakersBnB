298e555281bdac770d38b729498e29089d4ab448
KC Thomas <thisiskcthomas@gmail.com>
2020-10-30 11:18:35 +0000

    Integrating a lite sign-in feature. Passed feature tests.

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -38,7 +38,7 @@ end
 def sign_in_and_click
   visit '/'
     fill_in(:name, with: 'Bob')
-    fill_in(:user_name, with: 'Bob_007')
-    click_button('Submit')
+    fill_in(:sign_up_user_name, with: 'Bob_007')
+    click_button('Sign Up')
     click_link('New Property Listing')
 end
